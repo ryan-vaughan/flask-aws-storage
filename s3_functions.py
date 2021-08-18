e67199494aea385bbcd0cf28718e68af1381d510
Ryan Vaughan <ryan.vaughan@rackspace.com>
2021-08-18 10:47:38 -0500

    found issue

--- s3_functions.py
+++ s3_functions.py
@@ -1,18 +1,37 @@
 import boto3
+import boto3.session
 from botocore.client import Config
+import os
+import urllib3
 
-session = boto3.session.Session()
-region = session.region_name
+region2 = os.environ.get('REGION')
+print(region2)
+#REGION=$(curl http://169.254.169.254/latest/meta-data/placement/region)
+#session = boto3.session.Session()
+#region = session.region_name
 #region = 'us-east-2'
+client= boto3.client('s3')
+region = client.meta.region_name
+print(region)
+
+print("running 3")
+http = urllib3.PoolManager()
+r = http.request('GET', 'http://169.254.169.254/latest/meta-data/placement/region')
+#response = urllib.request.urlopen('https://169.254.169.254/latest/meta-data/placement/region')
+print(r.status)
+region3 = r.data
+region3 = region3.decode("utf-8")
+print(region3)
+
 
 def upload_file(file_name, bucket):
     object_name = file_name
-    s3_client = boto3.client('s3',endpoint_url=f'https://s3.{region}.amazonaws.com',config=Config(signature_version='s3v4',s3={'addressing_style': 'path'}))
+    s3_client = boto3.client('s3',endpoint_url=f'https://s3.{region3}.amazonaws.com',config=Config(signature_version='s3v4',s3={'addressing_style': 'path'}))
     response = s3_client.upload_file(file_name, bucket, object_name)
     return response
 
-def list_files(bucket):
-    s3_client = boto3.client('s3',endpoint_url=f'https://s3.{region}.amazonaws.com',config=Config(signature_version='s3v4',s3={'addressing_style': 'path'}))
+def list_files(bucket):                                                                             
+    s3_client = boto3.client('s3',endpoint_url=f'https://s3.{region3}.amazonaws.com',config=Config(signature_version='s3v4',s3={'addressing_style': 'path'}))
     contents = []
     try:
         for item in s3_client.list_objects(Bucket=bucket)['Contents']:
@@ -21,9 +40,9 @@ def list_files(bucket):
     except Exception as e:
         pass
     return contents
-
+                                                                                                    
 def show_image(bucket):
-    s3_client = boto3.client('s3',endpoint_url=f'https://s3.{region}.amazonaws.com',config=Config(signature_version='s3v4',s3={'addressing_style': 'path'}))
+    s3_client = boto3.client('s3',endpoint_url=f'https://s3.{region3}.amazonaws.com',config=Config(signature_version='s3v4',s3={'addressing_style': 'path'}))
     # location = boto3.client('s3').get_bucket_location(Bucket=bucket)['LocationConstraint']
     public_urls = []
     try:
